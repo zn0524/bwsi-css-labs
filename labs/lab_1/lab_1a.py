ce085e54ec6ac0c61be822f12f507ae7dd722cd9
zn0524 <96700305+zn0524@users.noreply.github.com>
2026-02-27 23:36:04 -0500

    Change robot speed from 5 m/s to 3 m/s

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -4,7 +4,7 @@ lab_1a.py
 The first lab in the BWSI CSS course. To complete this lab, fill out the variable on line 10
 with your name. Then, save the code, add it to the staging area, and commit it to the Git tree.
 
-This is to simulate a change made on a robot: robot_speed = 5 # m/s
+This is to simulate a change made on a robot: robot_speed = 3 # m/s
 """
 
 def main():
